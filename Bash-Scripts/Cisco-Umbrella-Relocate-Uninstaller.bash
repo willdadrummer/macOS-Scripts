e24d6248faaab5f3c1a79576746c352383e473d8
Will Daniel <52965466+willdadrummer@users.noreply.github.com>
2021-03-20 14:35:48 -0400

    commit

--- Bash-Scripts/Cisco-Umbrella-Relocate-Uninstaller.bash
+++ Bash-Scripts/Cisco-Umbrella-Relocate-Uninstaller.bash
@@ -11,14 +11,7 @@ DATA='<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-<key>APIFingerprint</key>
-<string>f32fac0151ac0cf0e45bdbe8e9093c99</string>
-<key>APIOrganizationID</key>
-<string>4324864</string>
-<key>APIUserID</key>
-<string>11444756</string>
-<key>InstallMenubar</key>
-<false/>
+
 </dict>
 </plist>'
 echo "$DATA" > "/Library/Application Support/OpenDNS Roaming Client/OrgInfo.plist"
